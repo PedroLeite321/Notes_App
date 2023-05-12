bb53b0ae4ec6d6afa87adadd89cc2800a1f01061
Pedro Kauã <pk.bleite@gmail.com>
2023-05-12 14:32:01 -0300

    delete edit button added

--- view/noteCol.php
+++ view/noteCol.php
@@ -1,5 +1,6 @@
 <div class="bg-success  mx-2 text-light  col-sm p-2 border rounded border-dark ">
-    <h2 class="p-4">Card title</h2>
+    <h2 class="fw-bolder p-4">Card title</h2>
     <p class="p-4">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <button type="button" class="btn btn-primary my-2 w-50">Edit</button>
+    <button type="button" class="btn btn-danger my-2 w-50">Delete</button>
 </div>
