6ef2b285090dcb6abcb0fb5837202c863178c68b
Pedro Kauã <pk.bleite@gmail.com>
2023-05-12 10:38:28 -0300

    Page Design-Starting

---
+++ view/noteCol.php
@@ -0,0 +1,5 @@
+<div class="bg-success text-light  col-sm p-4 border border-dark">
+    <h2 class="p-2">Card title</h2>
+    <p class="p-2">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
---
+++ view/noteRow.php
@@ -0,0 +1,8 @@
+<div class="container text-center w-75">
+    <div class="row gx-5">
+        <?php
+            require("./view/noteCol.php");
+            require("./view/noteCol.php");
+        ?>
+    </div>
+</div>
