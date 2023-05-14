4c98fb421cab30955a31d7b77067549434f61ba4
Pedro Kauã <pk.bleite@gmail.com>
2023-05-14 00:52:40 -0300

    modal Toggle fixed

--- view/html/modal.php
+++ view/html/modal.php
@@ -1,18 +1,18 @@
     <!-- Modal -->
-<div class="d-flex flex-wrap justify-content-center align-items-center modal fade modal-dialog modal-dialog-scrollable position-absolute vw-100 min-vw-100 min-vh-100 vh-100 mt-0 bg-transparent "  id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
-    <div class="modal-dialog">
+<div class="justify-content-center align-items-center modal modal-xl fade modal-dialog position-absolute vw-100 min-vw-100 min-vh-100 vh-100 mt-0 bg-transparent" data-bs-backdrop="static" data-bs-keyboard='false' id="myModal" tabindex="1" aria-labelledby="myModalLabel" aria-hidden="true" >
+    <div class="modal-dialog " role="document">
         <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-            <form class="d-flex align-text-center flex-column " id="modalForm" action="" method="post">
-                <input class="mt-2" type="text" name="" id="inputModal1" placeholder="Titulo">
-                <input class="mt-2" type="text" name="" id="inputModal2" placeholder="Conteúdo da nota">
+            <form class="d-flex align-text-center flex-column align-items-center" id="modalForm" action="" method="post">
+                <input class="mt-2 rounded border border-dark" type="text" name="" id="inputModal1" placeholder="Titulo">
+                <input class="mt-2 rounded border border-dark" type="text" name="" id="inputModal2" placeholder="Conteúdo da nota">
             </form>
         </div>
-        <div class="modal-footer">
+        <div class="modal-footer justify-content-center">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <button type="button" class="btn btn-primary">Save changes</button>
         </div>
--- view/html/noteCol.view.php
+++ view/html/noteCol.view.php
@@ -1,6 +1,6 @@
-<div class="mx-2 text-light  col-sm p-2 border rounded border-dark " style="background: rgb(24,55,42);">
-    <h2 class="fw-bolder p-4">Card title</h2>
+<div class="mx-2 text-light  col-sm  border rounded border-dark p-4 " style="background: rgb(24,55,42);">
+    <h2 class="fw-bolder p-4 ">Card title</h2>
     <p class="p-4">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-    <button type="button" style="background: rgb(24,55,42);" class="btn btn-primary my-2 w-50">Edit</button>
-    <button type="button" style="background: rgb(24,55,42);" class="btn btn-danger my-2 w-50">Delete</button>
+    <button type="button" style="background: rgb(24,55,42);" class="btn btn-primary my-2 w-50 p-25">Edit</button>
+    <button type="button" style="background: rgb(24,55,42);" class="btn btn-danger my-2 w-50 p-25">Delete</button>
 </div>
