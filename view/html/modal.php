ee800a8c8485a88bb47a8c1087442771b1e63319
Pedro Kauã <pk.bleite@gmail.com>
2023-05-13 21:39:19 -0300

    new Item Modal

---
+++ view/html/modal.php
@@ -0,0 +1,21 @@
+    <!-- Modal -->
+<div class="d-flex flex-wrap justify-content-center align-items-center modal fade modal-dialog modal-dialog-scrollable position-absolute vw-100 min-vw-100 min-vh-100 vh-100 mt-0 bg-transparent "  id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form class="d-flex align-text-center flex-column " id="modalForm" action="" method="post">
+                <input class="mt-2" type="text" name="" id="inputModal1" placeholder="Titulo">
+                <input class="mt-2" type="text" name="" id="inputModal2" placeholder="Conteúdo da nota">
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+    </div>
+</div>
--- view/myNotes.php
+++ view/myNotes.php
@@ -1,10 +1,11 @@
 <?php require("./view/html/htmlHead.view.php")?>
 <?php require("./view/html/navBar.view.php")?>
+<?php require("./view/html/modal.php")?>
 <main>
   <div id="pizza" class="d-flex  align-items-center justify-content-center align-content mt-4  w-100">
     <?php require("./view/html/noteRow.view.php") ?>
     <div class="bg-success align-items-center justify-content-center align-content-center flex-column " id="filterBtn">
-        <button class="btn mb-5 bg-warning border border-dark" id="myButton">Add Note</button>
+        <button class="btn mb-5 bg-warning border border-dark" id="myButton" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Note</button>
         <button class="btn mt-5 bg-warning border border-dark" id="myButton1">Filter</button>
     </div>
   </div>
